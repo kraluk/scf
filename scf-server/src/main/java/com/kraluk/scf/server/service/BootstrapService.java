dfa9fb19d70b8009c491daa9db610779952b2ecd
Lukasz Krauzowicz <kraluk@gmail.com>
2016-10-06 19:49:16 +0200

    Added very simple BootstrapService.

---
+++ scf-server/src/main/java/com/kraluk/scf/server/service/BootstrapService.java
@@ -0,0 +1,32 @@
+package com.kraluk.scf.server.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+/**
+ * SCF's Bootstrap Service
+ *
+ * @author lukasz
+ */
+@Service
+@Slf4j
+class BootstrapService implements InitializingBean {
+
+    private final Environment environment;
+
+    @Autowired
+    public BootstrapService(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("Available profiles: '{}'", Arrays.toString(environment.getActiveProfiles()));
+    }
+}
---
+++ scf-server/src/test/java/com/kraluk/scf/server/service/BootstrapServiceTest.java
@@ -0,0 +1,22 @@
+package com.kraluk.scf.server.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootstrapServiceTest {
+
+    @Autowired
+    private BootstrapService bootstrapService;
+
+    @Test
+    public void dummyTest() {
+        assertTrue(bootstrapService != null);
+    }
+}
